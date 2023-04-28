a874c57dfc9a01c1c9b22debbadb512c8b40a535
jamie <pink.toadette@gmail.com>
2023-04-28 10:15:32 -0400

    use mins instead of days in calc date

--- lib/helpers/date_format.dart
+++ lib/helpers/date_format.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
 
-import 'package:fren_app/constants/constants.dart';
-
 bool isYesterday(d1, d2) {
   return (d1.day - 1) == d2.day && d1.month == d2.month && d1.year == d2.year;
 }
@@ -10,14 +8,14 @@ String formatDate(int epochDate) {
   final d1 = DateTime.now();
   final d2 = d1.subtract(const Duration(days: 1));
   final comparedDate = DateTime.fromMicrosecondsSinceEpoch(epochDate * 1000);
-  int daysDiff = d2.difference(comparedDate).inDays;
+  int minsDiff = d2.difference(comparedDate).inMinutes;
 
   bool isYes = isYesterday(d1, d2);
 
-  if (daysDiff <= 1 && isYes) {
+  if (minsDiff <= (24 * 60) && isYes) {
     return "Yesterday";
   }
-  if (daysDiff > 1) {
+  if (minsDiff > (24 * 60)) {
     String month = "${comparedDate.month}";
     String day = "${comparedDate.day}";
     if (comparedDate.month < 10) {
