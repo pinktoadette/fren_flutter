a920329bdcd4af2e92cc3ddd5d1cc15394e662f2
jamie <pink.toadette@gmail.com>
2023-05-25 15:16:34 -0400

    revise profile page

--- lib/screens/user/profile_screen.dart
+++ lib/screens/user/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:machi_app/api/machi/friend_api.dart';
 import 'package:machi_app/api/machi/timeline_api.dart';
+import 'package:machi_app/constants/constants.dart';
 import 'package:machi_app/controller/chatroom_controller.dart';
 import 'package:machi_app/datas/storyboard.dart';
 import 'package:machi_app/datas/user.dart';
@@ -8,6 +9,7 @@ import 'package:machi_app/helpers/app_localizations.dart';
 import 'package:machi_app/models/user_model.dart';
 import 'package:machi_app/widgets/common/avatar_initials.dart';
 import 'package:flutter/material.dart';
+import 'package:machi_app/widgets/common/frosted_app_bar.dart';
 import 'package:machi_app/widgets/common/no_data.dart';
 import 'package:machi_app/widgets/storyboard/storyboard_item_widget.dart';
 import 'package:get/get.dart';
@@ -81,28 +83,72 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     /// Initialization
     _i18n = AppLocalizations.of(context);
-    double height = MediaQuery.of(context).size.height;
+    Size size = MediaQuery.of(context).size;
+    double avatar = 80;
 
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-        actions: <Widget>[
-          // Check the current User ID
-          if (UserModel().user.userId != widget.user.userId)
-            IconButton(
-              icon: Icon(Iconsax.flag,
-                  color: Theme.of(context).primaryColor, size: 24),
-              // Report/Block profile dialog
-              onPressed: () => ReportDialog(userId: widget.user.userId).show(),
-            )
-        ],
-      ),
-      body: ScopedModelDescendant<UserModel>(
-          builder: (context, child, userModel) {
-        return Stack(
+        body: CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          snap: false,
+          floating: false,
+          leading: const BackButton(),
+          expandedHeight: 180.0,
+          flexibleSpace: LayoutBuilder(builder: (context, constraints) {
+            bool isAppBarExpanded = constraints.maxHeight >
+                kToolbarHeight + MediaQuery.of(context).padding.top;
+
+            return FlexibleSpaceBar(
+                titlePadding: EdgeInsetsDirectional.only(
+                  start: isAppBarExpanded ? 0.0 : 50.0,
+                  bottom: 16.0,
+                ),
+                title: isAppBarExpanded
+                    ? Row(children: [
+                        Container(
+                          width: avatar,
+                          height: avatar,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 8,
+                                  offset: const Offset(5, 15),
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .tertiaryContainer
+                                      .withOpacity(.6),
+                                  spreadRadius: -9)
+                            ],
+                          ),
+                          child: AvatarInitials(
+                            userId: widget.user.userId,
+                            username: widget.user.username,
+                            photoUrl: widget.user.userProfilePhoto,
+                          ),
+                        ),
+                        Flexible(
+                            child: SizedBox(
+                                width: size.width - avatar,
+                                height: 50,
+                                child: Text(
+                                  widget.user.username,
+                                  overflow: TextOverflow.fade,
+                                  style:
+                                      Theme.of(context).textTheme.displayMedium,
+                                )))
+                      ])
+                    : Text(
+                        widget.user.username,
+                        overflow: TextOverflow.fade,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ));
+          }),
+        ),
+        SliverToBoxAdapter(
+            child: Stack(
           children: [
             SingleChildScrollView(
               child: Column(
@@ -110,54 +156,36 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   if (UserModel().user.userStatus == "hidden")
-                    const Text("Your profile is hidden"),
+                    Container(
+                      padding: const EdgeInsets.only(left: 20),
+                      width: size.width,
+                      color: Colors.red,
+                      child: Text(
+                        _i18n.translate("profile_protected"),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
                   Container(
-                    padding: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.all(20.0),
                     child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        AvatarInitials(
-                          userId: widget.user.userId,
-                          username: widget.user.username,
-                          photoUrl: widget.user.userProfilePhoto,
-                        ),
-                        const SizedBox(width: 20),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.user.username,
-                              style: Theme.of(context).textTheme.headlineSmall,
-                            ),
-                            const SizedBox(height: 10),
-                            const Row(
-                              children: [
-                                Text("123 \nFollowes"),
-                                SizedBox(width: 10),
-                                Text("431 \nFollowing"),
-                              ],
-                            )
-                          ],
-                        )
+                        Text("123 \n" + _i18n.translate("followers")),
+                        const SizedBox(width: 50),
+                        Text("431 \n" + _i18n.translate("following")),
+                        const Spacer(),
+                        _followButton(),
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [_followButton(), _friendRequest(context)],
-                    ),
-                  ),
+
+                  const SizedBox(height: 10),
 
                   /// Profile details
                   Container(
                     padding: const EdgeInsets.only(left: 20.0, right: 20),
-                    height: height * 0.1,
                     child: Text(widget.user.userBio ?? "",
-                        style: Theme.of(context).textTheme.bodyMedium),
+                        overflow: TextOverflow.fade,
+                        style: Theme.of(context).textTheme.bodySmall),
                   ),
 
                   _userPost()
@@ -165,9 +193,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
             ),
           ],
-        );
-      }),
-    );
+        ))
+      ],
+    ));
   }
 
   Widget _userPost() {
@@ -187,6 +215,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
               return Container();
             }
             return ListView.builder(
+              physics: const ClampingScrollPhysics(),
+              shrinkWrap: true,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 Storyboard item = snapshot.data![index];
@@ -210,82 +240,82 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  Widget _friendRequest(BuildContext context) {
-    _i18n = AppLocalizations.of(context);
-    double width = MediaQuery.of(context).size.width;
+  // Widget _friendRequest(BuildContext context) {
+  //   _i18n = AppLocalizations.of(context);
+  //   double width = MediaQuery.of(context).size.width;
 
-    switch (friendStatus["status"]) {
-      case 'REQUEST':
-        if (friendStatus["isRequester"] == 1) {
-          return OutlinedButton(
-              onPressed: null,
-              child: Text(
-                _i18n.translate("friend_sent"),
-                style: Theme.of(context).textTheme.labelSmall,
-              ));
-        }
-        return Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: width - 130,
-                  child: Flexible(
-                    fit: FlexFit.tight,
-                    child: Text(_i18n.translate("friend_other_sent"),
-                        style: Theme.of(context).textTheme.bodySmall),
-                  ),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          _friendApi
-                              .respondRequest(
-                                  widget.user.userId, FriendStatus.active)
-                              .then((_) => {_isUserFriend()});
-                        },
-                        child: Text(
-                          _i18n.translate("friend_accept_request"),
-                          style: const TextStyle(fontSize: 12),
-                        )),
-                    OutlinedButton(
-                        onPressed: () {
-                          _friendApi
-                              .respondRequest(
-                                  widget.user.userId, FriendStatus.unfriend)
-                              .then((_) => {_isUserFriend()});
-                        },
-                        child: Text(
-                          _i18n.translate("friend_reject_request"),
-                          style: const TextStyle(fontSize: 12),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)))),
-                  ],
-                )
-              ],
-            )
-          ],
-        );
-      case 'ACTIVE':
-        return const SizedBox.shrink();
-      case 'BLOCK':
-        return const SizedBox.shrink();
-      default:
-        return OutlinedButton.icon(
-          onPressed: () async {
-            _friendApi
-                .sendRequest(widget.user.userId)
-                .then((_) => {_isUserFriend()});
-          },
-          icon: const Icon(Iconsax.message),
-          label: Text(_i18n.translate("friend_send_request")),
-        );
-    }
-  }
+  //   switch (friendStatus["status"]) {
+  //     case 'REQUEST':
+  //       if (friendStatus["isRequester"] == 1) {
+  //         return OutlinedButton(
+  //             onPressed: null,
+  //             child: Text(
+  //               _i18n.translate("friend_sent"),
+  //               style: Theme.of(context).textTheme.labelSmall,
+  //             ));
+  //       }
+  //       return Row(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: [
+  //           Column(
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: [
+  //               SizedBox(
+  //                 width: width - 130,
+  //                 child: Flexible(
+  //                   fit: FlexFit.tight,
+  //                   child: Text(_i18n.translate("friend_other_sent"),
+  //                       style: Theme.of(context).textTheme.bodySmall),
+  //                 ),
+  //               ),
+  //               Row(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   ElevatedButton(
+  //                       onPressed: () {
+  //                         _friendApi
+  //                             .respondRequest(
+  //                                 widget.user.userId, FriendStatus.active)
+  //                             .then((_) => {_isUserFriend()});
+  //                       },
+  //                       child: Text(
+  //                         _i18n.translate("friend_accept_request"),
+  //                         style: const TextStyle(fontSize: 12),
+  //                       )),
+  //                   OutlinedButton(
+  //                       onPressed: () {
+  //                         _friendApi
+  //                             .respondRequest(
+  //                                 widget.user.userId, FriendStatus.unfriend)
+  //                             .then((_) => {_isUserFriend()});
+  //                       },
+  //                       child: Text(
+  //                         _i18n.translate("friend_reject_request"),
+  //                         style: const TextStyle(fontSize: 12),
+  //                       ),
+  //                       style: OutlinedButton.styleFrom(
+  //                           shape: RoundedRectangleBorder(
+  //                               borderRadius: BorderRadius.circular(50)))),
+  //                 ],
+  //               )
+  //             ],
+  //           )
+  //         ],
+  //       );
+  //     case 'ACTIVE':
+  //       return const SizedBox.shrink();
+  //     case 'BLOCK':
+  //       return const SizedBox.shrink();
+  //     default:
+  //       return OutlinedButton.icon(
+  //         onPressed: () async {
+  //           _friendApi
+  //               .sendRequest(widget.user.userId)
+  //               .then((_) => {_isUserFriend()});
+  //         },
+  //         icon: const Icon(Iconsax.message),
+  //         label: Text(_i18n.translate("friend_send_request")),
+  //       );
+  //   }
+  // }
 }
