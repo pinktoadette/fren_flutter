0ea29f489b65071287020c16d522044857f8c3a4
jamie <pink.toadette@gmail.com>
2023-05-29 14:44:49 -0400

    add astronatur

--- lib/widgets/discover_card.dart
+++ lib/widgets/discover_card.dart
@@ -26,14 +26,15 @@ class DiscoverCard extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Card(
+      color: Colors.black,
       child: SizedBox(
         height: screenHeight - 200,
         width: screenWidth,
         child: Container(
-            padding: const EdgeInsets.all(40),
-            height: screenHeight * 0.85,
+            padding: const EdgeInsets.only(left: 40, right: 40),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   const SizedBox(
                     height: 20,
@@ -41,16 +42,16 @@ class DiscoverCard extends StatelessWidget {
                   Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      const SizedBox(height: 100),
-                      const Frankloader(),
+                      const SizedBox(height: 50),
+                      Image.asset('assets/images/face.jpg', width: screenWidth),
                       Text(
                         title,
                         style: Theme.of(context).textTheme.headlineSmall,
+                        textAlign: TextAlign.center,
                       ),
+                      const SizedBox(height: 20),
                       Text(subtitle),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 50),
                       ElevatedButton(
                           onPressed: () {
                             ButtonChanged(true).dispatch(context);
