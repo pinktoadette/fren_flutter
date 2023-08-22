d4732034f7470ba88615a11fab6ab92c5ea97cb5
Jaime Lynn <lynn.jaimee@gmail.com>
2023-08-22 06:23:00 -0400

    macos

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\lynnj\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\lynnj\Desktop\fren\fren_flutter"
+export "FLUTTER_ROOT=/Users/jaime/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jaime/Desktop/machi/fren_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=74"
+export "FLUTTER_BUILD_NUMBER=79"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
