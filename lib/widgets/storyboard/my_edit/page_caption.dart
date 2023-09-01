8d7310e402204376af20b725c00307c09879d0da
Jaime Lynn <lynn.jaimee@gmail.com>
2023-09-01 08:48:55 -0400

    show scroll more indicator

--- lib/widgets/storyboard/my_edit/page_caption.dart
+++ lib/widgets/storyboard/my_edit/page_caption.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:lottie/lottie.dart';
 import 'package:machi_app/constants/constants.dart';
 import 'package:machi_app/controller/storyboard_controller.dart';
 import 'package:machi_app/datas/add_edit_text.dart';
@@ -48,33 +49,46 @@ class _PageTextCaptionState extends State<PageTextCaption> {
 
   @override
   Widget build(BuildContext context) {
+    String text = widget.script.text ?? "";
     return Container(
-        padding:
-            const EdgeInsets.only(left: 20, top: 20, right: 50, bottom: 10),
         width: size.width,
-        height: 150,
+        height: 170,
         color: const Color.fromARGB(255, 20, 20, 20).withAlpha(130),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.script.text ?? "",
-                style: const TextStyle(color: APP_INVERSE_PRIMARY_COLOR),
-              ),
-              if (widget.isPublished == false)
-                IconButton(
-                    onPressed: () {
-                      _onPageEditText();
-                    },
-                    icon: const Icon(
-                      Iconsax.edit,
-                      size: 20,
-                      color: APP_INVERSE_PRIMARY_COLOR,
-                    ))
-            ],
-          ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+                padding: const EdgeInsets.only(
+                    left: 20, top: 20, right: 0, bottom: 10),
+                width: size.width - 40,
+                height: 170,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        text,
+                        style:
+                            const TextStyle(color: APP_INVERSE_PRIMARY_COLOR),
+                      ),
+                      if (widget.isPublished == false)
+                        IconButton(
+                            onPressed: () {
+                              _onPageEditText();
+                            },
+                            icon: const Icon(
+                              Iconsax.edit,
+                              size: 20,
+                              color: APP_INVERSE_PRIMARY_COLOR,
+                            ))
+                    ],
+                  ),
+                )),
+            if (text.length > 200)
+              Lottie.asset('assets/lottie/down_arrow.json',
+                  width: 20, height: 20, repeat: false),
+          ],
         ));
   }
 
